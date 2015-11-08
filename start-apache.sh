a1e37fb8623f3ad2a131928f99485cdf7493cc49
disaster37 <linuxworkgroup@hotmail.com>
2015-11-08 16:46:02 +0100

    Update start-apache.sh

--- start-apache.sh
+++ start-apache.sh
@@ -13,7 +13,7 @@ else
   cp -f /etc/apache2/envvars /etc/apache2/envvars.orig
 fi
 
-[[ -z "$ENV_DOCKER_REGISTRY_HOST" ]] && die "Missing environment variable: ENV_DOCKER_REGISTRY_HOST=url-to-your-registry" 
+[[ -z "$ENV_DOCKER_REGISTRY_HOST" ]] && ENV_DOCKER_REGISTRY_HOST=registry 
 [[ -z "$ENV_DOCKER_REGISTRY_PORT" ]] && ENV_DOCKER_REGISTRY_PORT=80 
 [[ -z "$ENV_REGISTRY_PROXY_FQDN" ]] && ENV_REGISTRY_PROXY_FQDN=$ENV_DOCKER_REGISTRY_HOST
 [[ -z "$ENV_REGISTRY_PROXY_PORT" ]] && ENV_REGISTRY_PROXY_PORT=$ENV_DOCKER_REGISTRY_PORT
